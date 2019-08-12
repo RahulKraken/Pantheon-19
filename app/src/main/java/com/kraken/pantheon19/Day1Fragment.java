a21186ce2a96b42f36de5dfef4ebddffa81b2d61
ad12dec <ad12121997@gmail.com>
2019-08-12 16:05:16 +0530

    Splash, Homepage and Fragments

---
+++ app/src/main/java/com/kraken/pantheon19/Day1Fragment.java
@@ -0,0 +1,20 @@
+package com.kraken.pantheon19;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Day1Fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_day1,container,false);
+    }
+}
+
