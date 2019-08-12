95d07d88a37f531fa955067a583cf97499d33dd3
coderator143 <codebustershan.619@gmail.com>
2019-08-12 20:44:50 +0530

    Intents n cardview

--- app/src/main/java/com/kraken/pantheon19/HomepageActivity.java
+++ app/src/main/java/com/kraken/pantheon19/HomepageActivity.java
@@ -1,21 +1,29 @@
 package com.kraken.pantheon19;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomepageActivity extends AppCompatActivity {
 
     Button eventsButton;
+    ImageView facebook,instagram,web;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
         eventsButton=findViewById(R.id.events_button);
+        facebook=findViewById(R.id.facebook);
+        instagram=findViewById(R.id.instagram);
+        web=findViewById(R.id.web);
         eventsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,5 +31,37 @@ public class HomepageActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFacebookIntent();
+            }
+        });
+        instagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openInstagramIntent();
+            }
+        });
+    }
+
+    public void openFacebookIntent() {
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/860738913956082"));
+            startActivity(intent);
+        } catch(Exception e) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.facebook.com/860738913956082")));
+        }
+    }
+
+    public void openInstagramIntent() {
+        Uri uri = Uri.parse("https://instagram.com/pantheon_techfest?igshid=vyj3srrytza5");
+        Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+        likeIng.setPackage("com.instagram.android");
+        try {
+            startActivity(likeIng);
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("https://instagram.com/pantheon_techfest?igshid=vyj3srrytza5")));
+        }
     }
 }
