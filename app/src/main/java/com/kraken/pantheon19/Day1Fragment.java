96d6bb029383e0f2109c1f8b8f509100a7ae2d36
coderator143 <codebustershan.619@gmail.com>
2019-08-13 04:38:54 +0530

    cardview layout

--- app/src/main/java/com/kraken/pantheon19/Day1Fragment.java
+++ app/src/main/java/com/kraken/pantheon19/Day1Fragment.java
@@ -4,53 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import com.kraken.pantheon19.Events.Event;
-import com.kraken.pantheon19.Events.EventAdapter;
-
-import java.util.ArrayList;
 
 public class Day1Fragment extends Fragment {
-    private RecyclerView recyclerView;
-    private EventAdapter adapter;
-    private ArrayList<Event> eventsArrayList;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View rootview=inflater.inflate(R.layout.activity_day1,container,false);
-        recyclerView = rootview.findViewById(R.id.recyclerView1);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        eventsArrayList = new ArrayList<>();
-        adapter = new EventAdapter(getContext(), eventsArrayList);
-        recyclerView.setAdapter(adapter);
-        createListData();
-        return rootview;
-    }
-    private void createListData() {
-        Event event = new Event("Event 1", "This is Event 1","1-2 Hours","10:00 A.M");
-        eventsArrayList.add(event);
-        event = new Event("Event 2", "This is Event 2","1-2 Hours","11:00 A.M");
-        eventsArrayList.add(event);
-        event = new Event("Event 3", "This is Event 3","1-2 Hours","12:00 P.M");
-        eventsArrayList.add(event);
-        event = new Event("Event 4", "This is Event 4","1-2 Hours","1:00 P.M");
-        eventsArrayList.add(event);
-        event = new Event("Event 5", "This is Event 5","1-2 Hours","2:00 P.M");
-        eventsArrayList.add(event);
-        event = new Event("Event 6", "This is Event 6","1-2 Hours","3:00 P.M");
-        eventsArrayList.add(event);
-        event = new Event("Event 7", "This is Event 7","1-2 Hours","4:00 P.M");
-        eventsArrayList.add(event);
-        event = new Event("Event 8", "This is Event 8","1-2 Hours","5:00 P.M");
-        eventsArrayList.add(event);
-        event = new Event("Event 9", "This is Event 9","1-2 Hours","6:00 P.M");
-        eventsArrayList.add(event);
-        adapter.notifyDataSetChanged();
+        return inflater.inflate(R.layout.activity_day1,container,false);
     }
 }
 
--- app/src/main/java/com/kraken/pantheon19/HomepageActivity.java
+++ app/src/main/java/com/kraken/pantheon19/HomepageActivity.java
@@ -1,30 +1,31 @@
 package com.kraken.pantheon19;
 
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import in.goodiebag.carouselpicker.CarouselPicker;
 
 public class HomepageActivity extends AppCompatActivity {
 
     Button eventsButton;
-    ImageView facebook,instagram,youtube,web;
+    CarouselPicker carouselPicker;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        carouselPicker = (CarouselPicker)findViewById(R.id.carouselPicker);
         eventsButton=findViewById(R.id.events_button);
-        facebook=findViewById(R.id.facebook);
-        instagram=findViewById(R.id.instagram);
-        youtube=findViewById(R.id.youtube);
-        web=findViewById(R.id.web);
         eventsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,37 +33,41 @@ public class HomepageActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        facebook.setOnClickListener(new View.OnClickListener() {
+
+        List<CarouselPicker.PickerItem> imageItems = new ArrayList<>();
+        imageItems.add(new CarouselPicker.DrawableItem(R.drawable.i1));
+        imageItems.add(new CarouselPicker.DrawableItem(R.drawable.i2));
+        imageItems.add(new CarouselPicker.DrawableItem(R.drawable.i3));
+        //Creating an adapter
+        CarouselPicker.CarouselViewAdapter imageAdapter = new CarouselPicker.CarouselViewAdapter(this, imageItems, 0);
+        //Setting the adapter
+        carouselPicker.setAdapter(imageAdapter);
+
+
+
+
+        carouselPicker.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
-            public void onClick(View view) {
-                openFacebookIntent();
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                //position of the selected item
+                if(position == 0){
+                    Toast.makeText(HomepageActivity.this, position+" was clicked", Toast.LENGTH_SHORT).show();
+                }
+                else if(position == 1){
+                    Toast.makeText(HomepageActivity.this, position+" was clicked", Toast.LENGTH_SHORT).show();
+                }
+                // Same conditions for another cases.
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
             }
         });
-        instagram.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openInstagramIntent();
-            }
-        });
-    }
-
-    public void openFacebookIntent() {
-        try {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/860738913956082"));
-            startActivity(intent);
-        } catch(Exception e) {
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.facebook.com/860738913956082")));
-        }
-    }
-
-    public void openInstagramIntent() {
-        Uri uri = Uri.parse("https://instagram.com/pantheon_techfest?igshid=vyj3srrytza5");
-        Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
-        likeIng.setPackage("com.instagram.android");
-        try {
-            startActivity(likeIng);
-        } catch (ActivityNotFoundException e) {
-            startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("https://instagram.com/pantheon_techfest?igshid=vyj3srrytza5")));
-        }
     }
 }
