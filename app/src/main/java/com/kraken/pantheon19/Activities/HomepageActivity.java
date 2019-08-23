4a9383020a988576815e698d5ac5f9df9fcb6a48
RahulKraken <r.rahul4511@gmail.com>
2019-08-23 11:00:09 +0530

    home page layout done

--- app/src/main/java/com/kraken/pantheon19/Activities/HomepageActivity.java
+++ app/src/main/java/com/kraken/pantheon19/Activities/HomepageActivity.java
@@ -1,9 +1,7 @@
 package com.kraken.pantheon19.Activities;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,19 +9,9 @@ import com.kraken.pantheon19.R;
 
 public class HomepageActivity extends AppCompatActivity {
 
-    Button eventsButton;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-        eventsButton=findViewById(R.id.events_button);
-        eventsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(HomepageActivity.this,EventsActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
