b93c36e5da968b2730549ccf9a31d7247ac78357
coderator143 <codebustershan.619@gmail.com>
2019-09-08 16:14:18 +0530

    Layout done without theme; app working

--- app/src/main/java/com/kraken/pantheon19/Activities/EventsActivity.java
+++ app/src/main/java/com/kraken/pantheon19/Activities/EventsActivity.java
@@ -1,6 +1,7 @@
 package com.kraken.pantheon19.Activities;
 
-import android.os.Bundle;;
+import android.os.Bundle;
+import android.util.Log;;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
@@ -19,25 +20,21 @@ public class EventsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
-
         Toolbar toolbar = findViewById(R.id.app_bar);
-        TabLayout tabLayout = findViewById(R.id.tab_layout);
-        ViewPager viewPager = findViewById(R.id.pager);
-
         setSupportActionBar(toolbar);
-
         if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle("EVENTS");
+            getSupportActionBar().setTitle("Events");
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
-
+        TabLayout tabLayout=findViewById(R.id.tab_layout);
+        ViewPager viewPager=findViewById(R.id.pager);
         viewPager.setAdapter(new SectionPagerAdapter(getSupportFragmentManager()));
         tabLayout.setupWithViewPager(viewPager);
     }
-
     @Override
     public boolean onSupportNavigateUp() {
+        Log.d("upupdowndown", "onSupportNavigateUp: back pressed");
         finish();
         return true;
     }
--- app/src/main/java/com/kraken/pantheon19/Activities/SponsorsActivity.java
+++ app/src/main/java/com/kraken/pantheon19/Activities/SponsorsActivity.java
@@ -6,46 +6,48 @@ import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.PagerSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.SnapHelper;
 
-import com.kraken.pantheon19.Adapters.FlagshipRecyclerViewAdapter;
+import com.kraken.pantheon19.Adapters.SponsorsRecyclerViewAdapter;
 import com.kraken.pantheon19.Entities.Event;
 import com.kraken.pantheon19.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class FlagshipEventActivity extends AppCompatActivity {
+public class SponsorsActivity extends AppCompatActivity {
 
-    //SharedPrefThemes sharedPrefThemes;
-    private static final String TAG = "FlagshipEventActivity";
+    private static final String TAG = "SponsorsEventActivity";
     private RecyclerView recyclerView;
     private List<Event> events;
+    //SharedPrefThemes sharedPrefThemes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*sharedPrefThemes=new SharedPrefThemes(this);
-        if(sharedPrefThemes.loadNightModeState()) setTheme(R.style.AppTheme);
-        else setTheme(R.style.LightTheme);*/
-        setContentView(R.layout.activity_flagship_event);
+
+        setContentView(R.layout.activity_sponsors);
 
         // TODO : app bar
         Toolbar toolbar = findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
         if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle(getResources().getString(R.string.flagship_btn_desc));
+            getSupportActionBar().setTitle("Sponsors");
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
-        recyclerView = findViewById(R.id.flagship_recycler_view);
+        recyclerView = findViewById(R.id.sponsors_recycler_view);
 
         setupEventList();
         setupRecyclerView();
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        Log.d(TAG, "onSupportNavigateUp: back pressed");
+        finish();
+        return true;
     }
 
     /*
@@ -71,16 +73,12 @@ public class FlagshipEventActivity extends AppCompatActivity {
     }
 
     private void setupRecyclerView() {
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
-        FlagshipRecyclerViewAdapter adapter = new FlagshipRecyclerViewAdapter(this, events);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        SponsorsRecyclerViewAdapter adapter = new SponsorsRecyclerViewAdapter(this, events);
 
         // set on recycler view
-        Log.d(TAG, "setupRecyclerView: inflating recycler view");
+        Log.d(TAG, "setupRecyclerView: inflating speakers recycler view");
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
-
-        // snap behavior for recycler view
-        SnapHelper snapHelper = new PagerSnapHelper();
-        snapHelper.attachToRecyclerView(recyclerView);
     }
 }
