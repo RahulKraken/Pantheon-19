91fcfe1fb7b3056786d6882222e5c8b4d5ef79c9
RahulKraken <r.rahul4511@gmail.com>
2019-09-08 20:29:02 +0530

    sponsors page done

--- app/src/main/java/com/kraken/pantheon19/Activities/SponsorsActivity.java
+++ app/src/main/java/com/kraken/pantheon19/Activities/SponsorsActivity.java
@@ -1,16 +1,86 @@
 package com.kraken.pantheon19.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.kraken.pantheon19.Adapters.SponsorsRecyclerViewAdapter;
+import com.kraken.pantheon19.Entities.Event;
 import com.kraken.pantheon19.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SponsorsActivity extends AppCompatActivity {
+
+    private static final String TAG = "SponsorsEventActivity";
+    private RecyclerView recyclerView;
+    private List<Event> events;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sponsers);
+
+        setContentView(R.layout.activity_sponsors);
+
+        // TODO : app bar
+        Toolbar toolbar = findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Sponsors");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        recyclerView = findViewById(R.id.sponsors_recycler_view);
+
+        setupEventList();
+        setupRecyclerView();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        Log.d(TAG, "onSupportNavigateUp: back pressed");
+        finish();
+        return true;
+    }
+
+    /*
+    create list of events
+    TODO : replace with repository call
+     */
+    private void setupEventList() {
+        events = new ArrayList<>();
+        events.add(new Event(85, "kjdsfl", "skdjf", "kdsjfl", "10",
+                "kldsj", 2, 4, "lkfsjdflks", 0));
+        events.add(new Event(85, "kjdsfl", "skdjf", "kdsjfl", "ksdjfl","10",
+                2, 4, "kfakdjal", 0));
+        events.add(new Event(85, "kjdsfl", "skdjf", "kdsjfl", "ksdjfl","10",
+                2, 4, "kfakdjal", 0));
+        events.add(new Event(85, "kjdsfl", "skdjf", "kdsjfl", "ksdjfl","10",
+                2, 4, "kfakdjal", 0));
+        events.add(new Event(85, "kjdsfl", "skdjf", "kdsjfl", "ksdjfl","10",
+                2, 4, "kfakdjal", 0));
+        events.add(new Event(85, "kjdsfl", "skdjf", "kdsjfl", "ksdjfl","10",
+                2, 4, "kfakdjal", 0));
+        events.add(new Event(85, "kjdsfl", "skdjf", "kdsjfl", "ksdjfl","10",
+                2, 4, "kfakdjal", 0));
+        events.add(new Event(85, "kjdsfl", "skdjf", "kdsjfl", "ksdjfl","10",
+                2, 4, "kfakdjal", 0));
+    }
+
+    private void setupRecyclerView() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        SponsorsRecyclerViewAdapter adapter = new SponsorsRecyclerViewAdapter(this, events);
+
+        // set on recycler view
+        Log.d(TAG, "setupRecyclerView: inflating speakers recycler view");
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
     }
 }
