95f9f3bfd10992354814cbb17ffacd432566324f
RahulKraken <r.rahul4511@gmail.com>
2019-09-08 22:00:44 +0530

    added swipe refresh in leaderboard

--- app/src/main/java/com/kraken/pantheon19/Activities/LeaderboardActivity.java
+++ app/src/main/java/com/kraken/pantheon19/Activities/LeaderboardActivity.java
@@ -2,11 +2,13 @@ package com.kraken.pantheon19.Activities;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.kraken.pantheon19.Adapters.LeaderBoardRecyclerViewAdapter;
 import com.kraken.pantheon19.Entities.Team;
@@ -15,18 +17,22 @@ import com.kraken.pantheon19.R;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LeaderboardActivity extends AppCompatActivity {
+public class LeaderboardActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
     private static final String TAG = "LeaderBoardTeamActivity";
     private RecyclerView recyclerView;
     private List<Team> teams;
-    //SharedPrefThemes sharedPrefThemes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
 
-        // TODO : app bar
+        // setup swipe refresh
+        SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.leader_board_swipe_refresh_layout);
+        swipeRefreshLayout.setColorSchemeResources(R.color.md_red_500, R.color.md_green_500, R.color.md_blue_500, R.color.md_yellow_500);
+        swipeRefreshLayout.setOnRefreshListener(this);
+
+        // adding app bar
         Toolbar toolbar = findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
@@ -41,6 +47,13 @@ public class LeaderboardActivity extends AppCompatActivity {
         setupTeamList();
         setupRecyclerView();
     }
+
+    @Override
+    public void onRefresh() {
+        // TODO : fetch latest leader board
+        Toast.makeText(this, "Refreshing", Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     public boolean onSupportNavigateUp() {
         Log.d(TAG, "onSupportNavigateUp: back pressed");
@@ -57,12 +70,21 @@ public class LeaderboardActivity extends AppCompatActivity {
         teams.add(new Team(85, "amit", "10", "20"));
         teams.add(new Team(85, "1madarchod", "10", "20"));
         teams.add(new Team(85, "2madarchod", "10", "20"));
-        teams.add(new Team(85, "3madarchod",  "10", "20"));
-        teams.add(new Team(85, "4madarchod",  "10", "20"));
+        teams.add(new Team(85, "3madarchod", "10", "20"));
+        teams.add(new Team(85, "4madarchod", "10", "20"));
         teams.add(new Team(85, "5madarchod", "10", "20"));
         teams.add(new Team(85, "6madarchod", "10", "20"));
         teams.add(new Team(85, "7madarchod", "10", "20"));
         teams.add(new Team(85, "8madarchod", "10", "20"));
+        teams.add(new Team(85, "9madarchod", "10", "20"));
+        teams.add(new Team(85, "0madarchod", "10", "20"));
+        teams.add(new Team(85, "1madarchod", "10", "20"));
+        teams.add(new Team(85, "2madarchod", "10", "20"));
+        teams.add(new Team(85, "3madarchod", "10", "20"));
+        teams.add(new Team(85, "4madarchod", "10", "20"));
+        teams.add(new Team(85, "5madarchod", "10", "20"));
+        teams.add(new Team(85, "6madarchod", "10", "20"));
+        teams.add(new Team(85, "7madarchod", "10", "20"));
     }
 
     private void setupRecyclerView() {
